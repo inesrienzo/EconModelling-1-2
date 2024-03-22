2281cf7509b35ed9eb23c6bc0b4ba6e0b83af38c
inesrienzo <irienzo.ieu2022@student.ie.edu>
2024-03-22 10:08:19 +0100

    r

--- session1 2.py
+++ session1 2.py
@@ -1,10 +1,49 @@
+import random
 class Point:
     def _init_(self, x, y):
+        """
+        This will be called when instantiating an object
+        :param x: the value of x
+        :param y: the value of y
+        """
         self.x = x
         self.y = y
+    def _str_(self):
+        """
+        This will return the string value used in printing the point
+        :return:
+        """
+        return f"({self.x}, {self.y})"
 
-a = Point(2, 3)
-b = Point(7, 9)
+    def _repr_(self):
+        """
+        This is when the point is in a list or other container
+        :return:
+        """
 
-print(f"a=({a.x},{a.y})")
-print(f"a=({b.x},{b.y})")
+        return self._str_()
+a = Point(2,3)
+b = Point(7,9)
+print(f"a=({a.x}, {a.y})")
+print(f"b=({b.x}, {b.y})")
+
+
+points  = []
+for _ in range(5):
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    random_point = Point(x, y)
+    points.append(random_point)
+
+# or in a single line like this
+points.append(Point(random.randint(-100, 100),
+                    random.randint(-100, 100)))
+
+for point in points:
+    print(f"p({point.x}, {point.y})")
+
+#try to print the first point
+print("printing a point value", points[0])
+print(points)
+a = point(3,4)
+print(f"distance origin a= {a.distance_origin}")
